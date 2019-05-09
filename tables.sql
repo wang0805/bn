7403c492dd5957354615f95530423cfa37d59a2b
wenhao <wh_wang@live.com>
2019-05-09 16:55:45 +0800

    alter 1 table of remarks

--- tables.sql
+++ tables.sql
@@ -1,83 +1,86 @@
-CREATE TABLE IF NOT EXISTS products (
-	id SERIAL PRIMARY KEY,
-	code TEXT,
-	name TEXT,
-	created_at TIMESTAMP DEFAULT now()
-);
+-- CREATE TABLE IF NOT EXISTS products (
+-- 	id SERIAL PRIMARY KEY,
+-- 	code TEXT,
+-- 	name TEXT,
+-- 	created_at TIMESTAMP DEFAULT now()
+-- );
 
-CREATE TABLE IF NOT EXISTS clients (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	address TEXT,
-	entity TEXT,
-	idb TEXT,
-	commission NUMERIC(5,4),
-	recap_emails TEXT,
-	invoice_emails TEXT,
-	invoice_freq INT,
-	created_at TIMESTAMP DEFAULT now()
-);
+-- CREATE TABLE IF NOT EXISTS clients (
+-- 	id SERIAL PRIMARY KEY,
+-- 	name TEXT,
+-- 	address TEXT,
+-- 	entity TEXT,
+-- 	idb TEXT,
+-- 	commission NUMERIC(5,4),
+-- 	recap_emails TEXT,
+-- 	invoice_emails TEXT,
+-- 	invoice_freq INT,
+-- 	created_at TIMESTAMP DEFAULT now()
+-- );
 
-CREATE TABLE IF NOT EXISTS traders (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-    client_id INT,
-	created_at TIMESTAMP DEFAULT now()
-);
+-- CREATE TABLE IF NOT EXISTS traders (
+-- 	id SERIAL PRIMARY KEY,
+-- 	name TEXT,
+--     client_id INT,
+-- 	created_at TIMESTAMP DEFAULT now()
+-- );
 
-CREATE TABLE IF NOT EXISTS accounts (
-	id SERIAL PRIMARY KEY,
-	account TEXT,
-    client_id INT,
-	created_at TIMESTAMP DEFAULT now()
-);
+-- CREATE TABLE IF NOT EXISTS accounts (
+-- 	id SERIAL PRIMARY KEY,
+-- 	account TEXT,
+--     client_id INT,
+-- 	created_at TIMESTAMP DEFAULT now()
+-- );
 
-CREATE TABLE IF NOT EXISTS instruments (
-	id SERIAL PRIMARY KEY,
-	code TEXT,
-	name TEXT
-);
+-- CREATE TABLE IF NOT EXISTS instruments (
+-- 	id SERIAL PRIMARY KEY,
+-- 	code TEXT,
+-- 	name TEXT
+-- );
 
-CREATE TABLE IF NOT EXISTS transactions (
-	id SERIAL PRIMARY KEY,
-	trade_date DATE NOT NULL,
-	trade_time TEXT,
-    s_client TEXT,
-	s_clientid INT,
-	b_clientid INT,
-	b_client TEXT,
-	s_account TEXT,
-	b_account TEXT,
-	s_idb TEXT,
-	b_idb TEXT,
-	s_trader TEXT,
-	b_trader TEXT,
-	s_commission NUMERIC(5,4),
-	b_commission NUMERIC(5,4),
-	price NUMERIC(5,2),
-	strike NUMERIC(5,2),
-	product TEXT,
-	qty INT,
-	contract TEXT,
-	instrument TEXT,
-	year INT,
-	deal_id INT,
-	s_user TEXT,
-	b_user TEXT,
-	created_by_id INT,
-	created_at TIMESTAMP DEFAULT now()
-);
+-- CREATE TABLE IF NOT EXISTS transactions (
+-- 	id SERIAL PRIMARY KEY,
+-- 	trade_date DATE NOT NULL,
+-- 	trade_time TEXT,
+--     s_client TEXT,
+-- 	s_clientid INT,
+-- 	b_clientid INT,
+-- 	b_client TEXT,
+-- 	s_account TEXT,
+-- 	b_account TEXT,
+-- 	s_idb TEXT,
+-- 	b_idb TEXT,
+-- 	s_trader TEXT,
+-- 	b_trader TEXT,
+-- 	s_commission NUMERIC(5,4),
+-- 	b_commission NUMERIC(5,4),
+-- 	price NUMERIC(5,2),
+-- 	strike NUMERIC(5,2),
+-- 	product TEXT,
+-- 	qty INT,
+-- 	contract TEXT,
+-- 	instrument TEXT,
+-- 	year INT,
+-- 	deal_id INT,
+-- 	s_user TEXT,
+-- 	b_user TEXT,
+-- 	created_by_id INT,
+-- 	created_at TIMESTAMP DEFAULT now()
+-- );
 
-CREATE TABLE IF NOT EXISTS users (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	password TEXT,
-	permissions TEXT [],
-	created_at TIMESTAMP DEFAULT now()
-);
+-- CREATE TABLE IF NOT EXISTS users (
+-- 	id SERIAL PRIMARY KEY,
+-- 	name TEXT,
+-- 	password TEXT,
+-- 	permissions TEXT [],
+-- 	created_at TIMESTAMP DEFAULT now()
+-- );
 
-ALTER TABLE traders ADD COLUMN telephone int;
-ALTER TABLE clients ADD COLUMN telephone int;
+-- ALTER TABLE traders ADD COLUMN telephone int;
+-- ALTER TABLE clients ADD COLUMN telephone int;
+-- ALTER TABLE transactions ADD COLUMN remarks TEXT;
+
+
 
 -- SELECT clients.id, clients.name, products.code, products.name, gcms.code, gcms.name, accounts.account, traders.name from clients 
 -- inner join clients_products 
