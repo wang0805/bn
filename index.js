2eaf282c5e6ed673809c9f80cbf40eab59719287
wenhao <wh_wang@live.com>
2019-08-05 13:39:04 +0800

    added limit 50mb to handle larger files

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ const app = express();
 
 // Set up middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // app.use(guard.check("user"));
 
