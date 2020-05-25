bbc873ce0435b58a1981ec14587165709acce541
wang0805 <wh_wang@live.com>
2020-05-25 17:07:51 +0800

    index.js email routes

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ var transporter = nodemailer.createTransport({
   secure: false, //true for 465 and false for other ports, gmail SSL 465 / TLS 587
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
+    pass: process.env.EMAIL_PASSWORD,
   },
-  tls: { rejectUnauthorized: false }
+  tls: { rejectUnauthorized: false },
 });
 
 const email = new Email({
   message: {
-    from: '"futuresops" <operations@bpifinancial.com>'
+    from: '"futuresops" <operations@bpifinancial.com>',
   },
   send: true,
-  transport: transporter
+  transport: transporter,
 });
 /**
  * ===================================
@@ -76,9 +76,9 @@ app.post("/sendpdf", (req, res) => {
       {
         filename: `invoice_${req.body.invoiceNo}.pdf`,
         path: __dirname + "/result.pdf",
-        contentType: "application/pdf"
-      }
-    ]
+        contentType: "application/pdf",
+      },
+    ],
   };
   //preview in my window
   previewEmail(mailOptions)
@@ -108,7 +108,7 @@ app.post("/createpdf", (req, res) => {
       top: "1.5cm",
       right: "1cm",
       bottom: "0.5cm",
-      left: "1cm"
+      left: "1cm",
     },
     paginationOffset: 1,
     header: {
@@ -118,7 +118,7 @@ app.post("/createpdf", (req, res) => {
       <img style="width: 90px; position: absolute; top: 0px; left: 30px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png">
       <div style="text-align: center; font-size: 10px;">3 Anson Road, #26-01 Springleaf Tower (S) 079909 TEL: (65) 64990618</div>
       <div style="text-align: center; font-size: 10px;">GST Registration No: 201724830E</div>
-      `
+      `,
     },
     footer: {
       height: "15mm",
@@ -127,10 +127,10 @@ app.post("/createpdf", (req, res) => {
         2: "<div style='font-size: 12px;'>2</div>",
         3: "<div style='font-size: 12px;'>3</div>",
         4: "<div style='font-size: 12px;'>4</div>",
-        last: "<div style='font-size: 12px;'>Last</div>"
-      }
+        last: "<div style='font-size: 12px;'>Last</div>",
+      },
     },
-    base: "file:///C:/Users/test/bpibackoffice/backend/documents/" // to be able to read images
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
   };
   let optionsHK = {
     orientation: "protrait",
@@ -139,7 +139,7 @@ app.post("/createpdf", (req, res) => {
       top: "1.5cm",
       right: "1cm",
       bottom: "0.5cm",
-      left: "1cm"
+      left: "1cm",
     },
     paginationOffset: 1,
     header: {
@@ -148,7 +148,7 @@ app.post("/createpdf", (req, res) => {
       <div style="text-align: center; font-size: 13px;">BRIGHT POINT INTERNATIONAL FUTURES LIMITED</div>
       <img style="width: 90px; position: absolute; top: 0px; left: 30px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png">
       <div style="text-align: center; font-size: 9px;">Units 3401-03, 34/F, China Merchants Tower, Shun Tak Centre, 168-200 Connaught Road Central, Sheung Wan, Hong Kong</div>
-      `
+      `,
     },
     footer: {
       height: "15mm",
@@ -157,25 +157,27 @@ app.post("/createpdf", (req, res) => {
         2: "<div style='font-size: 12px;'>2</div>",
         3: "<div style='font-size: 12px;'>3</div>",
         4: "<div style='font-size: 12px;'>4</div>",
-        last: "<div style='font-size: 12px;'>Last</div>"
-      }
+        last: "<div style='font-size: 12px;'>Last</div>",
+      },
     },
-    base: "file:///C:/Users/test/bpibackoffice/backend/documents/" // to be able to read images
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
   };
   if (req.body.client[0].entity === "SG") {
-    pdf.create(pdfTemplateSg(req.body), options).toFile("result.pdf", err => {
+    pdf.create(pdfTemplateSg(req.body), options).toFile("result.pdf", (err) => {
       if (err) {
         res.send(Promise.reject());
       }
       res.send(Promise.resolve()); //,then in client side
     });
   } else if (req.body.client[0].entity === "HK") {
-    pdf.create(pdfTemplateHk(req.body), optionsHK).toFile("result.pdf", err => {
-      if (err) {
-        res.send(Promise.reject());
-      }
-      res.send(Promise.resolve());
-    });
+    pdf
+      .create(pdfTemplateHk(req.body), optionsHK)
+      .toFile("result.pdf", (err) => {
+        if (err) {
+          res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+      });
   } else {
     res.send(Promise.reject());
   }
@@ -197,7 +199,7 @@ app.post("/createrecappdf", async (req, res) => {
       top: "1.5cm",
       right: "1cm",
       bottom: "0.5cm",
-      left: "1cm"
+      left: "1cm",
     },
     paginationOffset: 1,
     header: {
@@ -205,17 +207,17 @@ app.post("/createrecappdf", async (req, res) => {
       contents: `
       <div style="text-align: center; font-size: 15px;">BRIGHT POINT INTERNATIONAL FUTURES</div>   
       <img style="width: 90px; position: absolute; top: 0px; left: 90px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png">
-      `
+      `,
     },
     footer: {
       height: "20mm",
       contents: {
         first: `
         <div style="text-align: center; font-size: 12px;">Bright Point International Futures</div>
-        <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>  `
-      }
+        <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>  `,
+      },
     },
-    base: "file:///C:/Users/test/bpibackoffice/backend/documents/" // to be able to read images
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
   };
   let selloptions = {
     orientation: "protrait",
@@ -224,7 +226,7 @@ app.post("/createrecappdf", async (req, res) => {
       top: "1.5cm",
       right: "1cm",
       bottom: "0.5cm",
-      left: "1cm"
+      left: "1cm",
     },
     paginationOffset: 1,
     header: {
@@ -232,7 +234,7 @@ app.post("/createrecappdf", async (req, res) => {
       contents: `
       <div style="text-align: center; font-size: 15px;">BRIGHT POINT INTERNATIONAL FUTURES</div> 
       <img style="width: 90px; position: absolute; top: 0px; left: 90px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png"> 
-      `
+      `,
     },
     footer: {
       height: "20mm",
@@ -240,15 +242,15 @@ app.post("/createrecappdf", async (req, res) => {
         first: `      
         <div style="text-align: center; font-size: 12px;">Bright Point International Futures</div>
         <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>     
-        `
-      }
+        `,
+      },
     },
-    base: "file:///C:/Users/test/bpibackoffice/backend/documents/" // to be able to read images
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
   };
 
   await pdf
     .create(pdfRecapBuyer(req.body), buyoptions)
-    .toFile(`./recaps/recap_b_${req.body.tradeid}.pdf`, err => {
+    .toFile(`./recaps/recap_b_${req.body.tradeid}.pdf`, (err) => {
       if (err) {
         res.send(Promise.reject());
       } else {
@@ -257,7 +259,7 @@ app.post("/createrecappdf", async (req, res) => {
     });
   await pdf
     .create(pdfRecapSeller(req.body), selloptions)
-    .toFile(`./recaps/recap_s_${req.body.tradeid}.pdf`, err => {
+    .toFile(`./recaps/recap_s_${req.body.tradeid}.pdf`, (err) => {
       if (err) {
         res.send(Promise.reject());
       } else {
@@ -280,7 +282,7 @@ app.post("/send", async (req, res) => {
       top: "1.5cm",
       right: "1cm",
       bottom: "0.5cm",
-      left: "1cm"
+      left: "1cm",
     },
     paginationOffset: 1,
     header: {
@@ -288,17 +290,17 @@ app.post("/send", async (req, res) => {
       contents: `
       <div style="text-align: center; font-size: 15px;">BRIGHT POINT INTERNATIONAL FUTURES</div>   
       <img style="width: 90px; position: absolute; top: 0px; left: 90px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png">
-      `
+      `,
     },
     footer: {
       height: "20mm",
       contents: {
         first: `
         <div style="text-align: center; font-size: 12px;">Bright Point International Futures</div>
-        <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>  `
-      }
+        <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>  `,
+      },
     },
-    base: "file:///C:/Users/test/bpibackoffice/backend/documents/" // to be able to read images
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
   };
   let selloptions = {
     orientation: "protrait",
@@ -307,7 +309,7 @@ app.post("/send", async (req, res) => {
       top: "1.5cm",
       right: "1cm",
       bottom: "0.5cm",
-      left: "1cm"
+      left: "1cm",
     },
     paginationOffset: 1,
     header: {
@@ -315,7 +317,7 @@ app.post("/send", async (req, res) => {
       contents: `
       <div style="text-align: center; font-size: 15px;">BRIGHT POINT INTERNATIONAL FUTURES</div> 
       <img style="width: 90px; position: absolute; top: 0px; left: 90px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png"> 
-      `
+      `,
     },
     footer: {
       height: "20mm",
@@ -323,15 +325,15 @@ app.post("/send", async (req, res) => {
         first: `      
         <div style="text-align: center; font-size: 12px;">Bright Point International Futures</div>
         <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>     
-        `
-      }
+        `,
+      },
     },
-    base: "file:///C:/Users/test/bpibackoffice/backend/documents/" // to be able to read images
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
   };
 
   await pdf
     .create(pdfRecapBuyer(req.body), buyoptions)
-    .toFile(`./recaps/recap_b_${req.body.tradeid}.pdf`, err => {
+    .toFile(`./recaps/recap_b_${req.body.tradeid}.pdf`, (err) => {
       if (err) {
         res.send("Error in buyer recap");
       } else {
@@ -340,7 +342,7 @@ app.post("/send", async (req, res) => {
     });
   await pdf
     .create(pdfRecapSeller(req.body), selloptions)
-    .toFile(`./recaps/recap_s_${req.body.tradeid}.pdf`, err => {
+    .toFile(`./recaps/recap_s_${req.body.tradeid}.pdf`, (err) => {
       if (err) {
         res.send("Error in seller recap");
       } else {
@@ -389,9 +391,9 @@ app.post("/send", async (req, res) => {
       {
         filename: `Recap_${req.body.tradeid}.pdf`,
         path: __dirname + `/recaps/recap_b_${req.body.tradeid}.pdf`,
-        contentType: "application/pdf"
-      }
-    ]
+        contentType: "application/pdf",
+      },
+    ],
   };
 
   let sell_mailOptions = {
@@ -423,12 +425,12 @@ app.post("/send", async (req, res) => {
       {
         filename: `Recap_${req.body.tradeid}.pdf`,
         path: __dirname + `/recaps/recap_s_${req.body.tradeid}.pdf`,
-        contentType: "application/pdf"
-      }
-    ]
+        contentType: "application/pdf",
+      },
+    ],
   };
 
-  await setTimeout(function() {
+  await setTimeout(function () {
     transporter.sendMail(buy_mailOptions, (error, info) => {
       if (error) {
         return console.log(error);
@@ -442,6 +444,206 @@ app.post("/send", async (req, res) => {
         console.log("4th Message sent: %s", info.messageId);
         // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
       });
+    });
+  }, 2288);
+
+  res.send("success in sending mail");
+});
+
+app.post("/sendSeller", async (req, res) => {
+  // options to create pdf
+
+  let selloptions = {
+    orientation: "protrait",
+    format: "A4",
+    border: {
+      top: "1.5cm",
+      right: "1cm",
+      bottom: "0.5cm",
+      left: "1cm",
+    },
+    paginationOffset: 1,
+    header: {
+      height: "20mm",
+      contents: `
+      <div style="text-align: center; font-size: 15px;">BRIGHT POINT INTERNATIONAL FUTURES</div> 
+      <img style="width: 90px; position: absolute; top: 0px; left: 90px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png"> 
+      `,
+    },
+    footer: {
+      height: "20mm",
+      contents: {
+        first: `      
+        <div style="text-align: center; font-size: 12px;">Bright Point International Futures</div>
+        <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>     
+        `,
+      },
+    },
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
+  };
+
+  await pdf
+    .create(pdfRecapSeller(req.body), selloptions)
+    .toFile(`./recaps/recap_s_${req.body.tradeid}.pdf`, (err) => {
+      if (err) {
+        res.send("Error in seller recap");
+      } else {
+        console.log("Successful in creating seller recap");
+      }
+    });
+
+  //for emails
+  let size = 0;
+  let strike = "N/A";
+  if (req.body.instrument === "S") {
+    size = req.body.qty * 500 * parseInt(req.body.consMonth);
+  } else {
+    size = req.body.qty * 100 * parseInt(req.body.consMonth);
+  }
+  if (req.body.strike) {
+    strike = `USD ${req.body.strike}`;
+  }
+  //sending emails to clients
+  let sell_mailOptions = {
+    from: "operations@bpifinancial.com",
+    to: req.body.s_recap,
+    subject: `Recap ${req.body.s_client} BPI${req.body.tradeid}`,
+    html: `
+    <p>Dear ${req.body.s_client},</p>
+    <br/>
+    <p>Please see the trade details as follows:</p>
+    <p>Trade Id: BPI${req.body.tradeid}</p>
+    <p>Buy/Sell: Sell</p>
+    <p>Product: ${req.body.product_code}</p>
+    <p>Contract: ${req.body.contract}</p>
+    <p>Price: ${req.body.price}</p>
+    <p>Strike(if applicable): ${strike}</p>
+    <p>Instrument: ${req.body.instrument}</p>
+    <p>Quantity(lots): ${req.body.qty} lots/month</p>
+    <p>Quantity(MT): ${size} MT</p>
+    <p>Trader: ${req.body.s_trader}</p>
+    <p>Account: ${req.body.s_accounts}</p>
+    <p>Commission: Standard as Agreed</p>
+    <br/>
+    <p>Best Regards,</p>
+    <p>BPI Operations</p>
+    <p>Email: operations@bpifinancial.com</p>
+    `,
+    attachments: [
+      {
+        filename: `Recap_${req.body.tradeid}.pdf`,
+        path: __dirname + `/recaps/recap_s_${req.body.tradeid}.pdf`,
+        contentType: "application/pdf",
+      },
+    ],
+  };
+
+  await setTimeout(function () {
+    transporter.sendMail(sell_mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Seller Message sent: %s", info.messageId);
+      // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    });
+  }, 2288);
+
+  res.send("success in sending mail");
+});
+
+app.post("/sendBuyer", async (req, res) => {
+  // options to create pdf
+  let buyoptions = {
+    orientation: "protrait",
+    format: "A4",
+    border: {
+      top: "1.5cm",
+      right: "1cm",
+      bottom: "0.5cm",
+      left: "1cm",
+    },
+    paginationOffset: 1,
+    header: {
+      height: "20mm",
+      contents: `
+      <div style="text-align: center; font-size: 15px;">BRIGHT POINT INTERNATIONAL FUTURES</div>   
+      <img style="width: 90px; position: absolute; top: 0px; left: 90px;" src="file:///C:/Users/test/bpibackoffice/backend/documents/bpi.png">
+      `,
+    },
+    footer: {
+      height: "20mm",
+      contents: {
+        first: `
+        <div style="text-align: center; font-size: 12px;">Bright Point International Futures</div>
+        <div style="text-align: center; font-size: 12px;">SG | T: +65 6239 9293 | E: operations@bpifinancial.com</div>  `,
+      },
+    },
+    base: "file:///C:/Users/test/bpibackoffice/backend/documents/", // to be able to read images
+  };
+
+  await pdf
+    .create(pdfRecapBuyer(req.body), buyoptions)
+    .toFile(`./recaps/recap_b_${req.body.tradeid}.pdf`, (err) => {
+      if (err) {
+        res.send("Error in buyer recap");
+      } else {
+        console.log("Successful in creating buyer recap");
+      }
+    });
+
+  //for emails
+  let size = 0;
+  let strike = "N/A";
+  if (req.body.instrument === "S") {
+    size = req.body.qty * 500 * parseInt(req.body.consMonth);
+  } else {
+    size = req.body.qty * 100 * parseInt(req.body.consMonth);
+  }
+  if (req.body.strike) {
+    strike = `USD ${req.body.strike}`;
+  }
+  //sending emails to clients
+  let buy_mailOptions = {
+    from: "operations@bpifinancial.com",
+    to: req.body.b_recap,
+    subject: `Recap ${req.body.b_client} BPI${req.body.tradeid}`,
+    html: `
+    <p>Dear ${req.body.b_client},</p>
+    <br/>
+    <p>Please see the trade details as follows:</p>
+    <p>Trade Id: BPI${req.body.tradeid}</p>
+    <p>Buy/Sell: Buy</p>
+    <p>Product: ${req.body.product_code}</p>
+    <p>Contract: ${req.body.contract}</p>
+    <p>Price: ${req.body.price}</p>
+    <p>Strike(if applicable): ${strike}</p>
+    <p>Instrument: ${req.body.instrument}</p>
+    <p>Quantity(lots): ${req.body.qty} lots/month</p>
+    <p>Quantity(MT): ${size} MT</p>
+    <p>Trader: ${req.body.b_trader}</p>
+    <p>Account: ${req.body.b_accounts}</p>
+    <p>Commission: Standard as Agreed</p>
+    <br/>
+    <p>Best Regards,</p>
+    <p>BPI Operations</p>
+    <p>Email: operations@bpifinancial.com</p>
+    `,
+    attachments: [
+      {
+        filename: `Recap_${req.body.tradeid}.pdf`,
+        path: __dirname + `/recaps/recap_b_${req.body.tradeid}.pdf`,
+        contentType: "application/pdf",
+      },
+    ],
+  };
+
+  await setTimeout(function () {
+    transporter.sendMail(buy_mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("buyer Message sent: %s", info.messageId);
+      // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     });
   }, 2288);
 
